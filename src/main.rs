7d45d9cd8722c56ef73227eb10fc0e3b5bddad2d
Arkar Aung <arica.aung@gmail.com>
2021-07-25 15:46:47 +0800

    Add files

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+fn main() {
+    let server = HttpServer::new(|| {
+	App::new()
+	    .route("/", web::get().to(get_index))
+    });
+
+    println!("Serving on http://localhost:3000...");
+    server.bind("127.0.0.1:3000").expect("error binding server to address")
+	.run().expect("error running server");
+}
+
+fn get_index() -> HttpResponse {
+    HttpResponse:: Ok()
+	.content_type("text/html")
+	.body(
+	    r#"
+		<title>Test web server</title>
+		<p>Hello Rust Web Server</p>
+		"#,
+	)
+}
